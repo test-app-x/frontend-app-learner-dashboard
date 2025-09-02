fc81365a362e35a61b925e83359020cce7eeb9e8
esitehoster <esitehoster@gmail.com>
2025-09-02 23:38:50 +0500

    test

--- example.env.config.js
+++ example.env.config.js
@@ -1,73 +1,61 @@
-/*
-Learner Dashboard is now able to handle JS-based configuration!
+/* Basic Frontend Plugin Workshop Example. */
 
-For the time being, the `.env.*` files are still made available when cloning down this repo or pulling from
-the master branch. To switch to using `env.config.js`, make a copy of `example.env.config.js` and configure as needed.
+import {
+  DIRECT_PLUGIN,
+  PLUGIN_OPERATIONS,
+} from '@openedx/frontend-plugin-framework';
 
-For testing with Jest Snapshot, there is a mock in `/src/setupTest.jsx` for `getConfig` that will need to be
-uncommented.
+import {
+  Card,
+  Container,
+} from '@openedx/paragon';
 
-Note: having both .env and env.config.js files will follow a predictable order, in which non-empty values in the
-JS-based config will overwrite the .env environment variables.
+const sidebarRecommender = () => (
+  <Container>
+    <h3 className="p2-b">
+      Other courses for you
+    </h3>
+    <div className="product-card-container d-flex">
+      <Card className="mb-4" orientation="horizontal">
+        <Card.ImageCap
+          src="https://picsum.photos/360/200/"
+          srcAlt="Card image"
+          logoSrc="https://picsum.photos/150/150"
+          logoAlt="Card logo"
+        />
+        <Card.Section
+          title="Section title"
+        >
+          Course recommendation
+        </Card.Section>
+        <Card.Footer orientation="horizontal">
+        </Card.Footer>
+      </Card>
+    </div>
+  </Container>
+);
 
-frontend-platform's getConfig loads configuration in the following sequence:
-- .env file config
-- optional handlers (commonly used to merge MFE-specific config in via additional process.env variables)
-- env.config.js file config
-- runtime config
-*/
+import Footer from './src/my-brand/Footer'
 
-module.exports = {
-  NODE_ENV: 'development',
-  NODE_PATH: './src',
-  PORT: 1996,
-  BASE_URL: 'localhost:1996',
-  LMS_BASE_URL: 'http://localhost:18000',
-  ECOMMERCE_BASE_URL: 'http://localhost:18130',
-  CREDIT_PURCHASE_URL: 'http://localhost:8140',
-  LOGIN_URL: 'http://localhost:18000/login',
-  LOGOUT_URL: 'http://localhost:18000/logout',
-  LOGO_URL: 'https://edx-cdn.org/v3/default/logo.svg',
-  LOGO_TRADEMARK_URL: 'https://edx-cdn.org/v3/default/logo-trademark.svg',
-  LOGO_WHITE_URL: 'https://edx-cdn.org/v3/default/logo-white.svg',
-  FAVICON_URL: 'https://edx-cdn.org/v3/default/favicon.ico',
-  CSRF_TOKEN_API_PATH: '/csrf/api/v1/token',
-  REFRESH_ACCESS_TOKEN_ENDPOINT: 'http://localhost:18000/login_refresh',
-  ACCESS_TOKEN_COOKIE_NAME: 'edx-jwt-cookie-header-payload',
-  USER_INFO_COOKIE_NAME: 'edx-user-info',
-  SITE_NAME: 'localhost',
-  DATA_API_BASE_URL: 'http://localhost:8000',
-  // LMS_CLIENT_ID should match the lms DOT client application in your LMS container
-  LMS_CLIENT_ID: 'login-service-client-id',
-  SEGMENT_KEY: '',
-  FEATURE_FLAGS: {},
-  MARKETING_SITE_BASE_URL: 'http://localhost:18000',
-  SUPPORT_URL: 'http://localhost:18000/support',
-  CONTACT_URL: 'http://localhost:18000/contact',
-  OPEN_SOURCE_URL: 'http://localhost:18000/openedx',
-  TERMS_OF_SERVICE_URL: 'http://localhost:18000/terms-of-service',
-  PRIVACY_POLICY_URL: 'http://localhost:18000/privacy-policy',
-  FACEBOOK_URL: 'https://www.facebook.com',
-  TWITTER_URL: 'https://twitter.com',
-  YOU_TUBE_URL: 'https://www.youtube.com',
-  LINKED_IN_URL: 'https://www.linkedin.com',
-  REDDIT_URL: 'https://www.reddit.com',
-  APPLE_APP_STORE_URL: 'https://www.apple.com/ios/app-store/',
-  GOOGLE_PLAY_URL: 'https://play.google.com/store',
-  ENTERPRISE_MARKETING_URL: 'http://example.com',
-  ENTERPRISE_MARKETING_UTM_SOURCE: 'example.com',
-  ENTERPRISE_MARKETING_UTM_CAMPAIGN: 'example.com Referral',
-  ENTERPRISE_MARKETING_FOOTER_UTM_MEDIUM: 'Footer',
-  LEARNING_BASE_URL: 'http://localhost:2000',
-  SESSION_COOKIE_DOMAIN: 'localhost',
-  HOTJAR_APP_ID: '',
-  HOTJAR_VERSION: 6,
-  HOTJAR_DEBUG: '',
-  NEW_RELIC_APP_ID: '',
-  NEW_RELIC_LICENSE_KEY: '',
-  ACCOUNT_SETTINGS_URL: 'http://localhost:1997',
-  ACCOUNT_PROFILE_URL: 'http://localhost:1995',
-  ENABLE_NOTICES: '',
-  CAREER_LINK_URL: '',
-  EXPERIMENT_08_23_VAN_PAINTED_DOOR: true,
-};
+const config = {
+  pluginSlots: {
+
+    footer_slot: {
+      keepDefault: true,
+      plugins: [
+        {
+          op: PLUGIN_OPERATIONS.Insert,
+          widget: {
+            id: 'custom_footer',
+            type: DIRECT_PLUGIN,
+            RenderWidget: Footer,
+          },
+        }
+      ]
+    },
+
+
+  },
+}
+
+export default config;
